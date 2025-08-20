c522a16795e2ec69614e1abf90bb535d616c2295
Atalq <ahaq2@cam.ac.uk>
2025-08-20 15:06:03 +0100

    Drone MOVESSS!!!

--- 1.py
+++ 1.py
@@ -9,17 +9,17 @@ class DRONE:
 
         self.x = random.randint(0, 800)
         self.y = random.randint(0, 600)
-        self.speed = 1
+        self.speed = 5
         self.pos = Vector2(self.x, self.y)
-        self.direction = (1, 0)
+        self.direction = Vector2(1, 0)
 
-    def update():
-        pass
-
+    def move_drone(self):
+        
+        self.pos = Vector2(self.pos.x + self.speed * self.direction.x, self.pos.y + self.speed * self.direction.y)
 
 
     def draw_drone(self, screen):
-        drone_rect = pygame.Rect(int(self.pos.x), int(self.pos.y), 10, 15)
+        drone_rect = pygame.Rect(int(self.pos.x), int(self.pos.y), 10, 10)
         pygame.draw.rect(screen, BLACK, drone_rect)
         
 
@@ -28,11 +28,16 @@ class DRONE:
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-drone = DRONE()
+
 
 def main():
-        # Set up pygame and screen  
+        # Set up pygame 
     pygame.init()
+
+    # drone object creation
+    drone = DRONE()
+
+        # Screen set up
     screen_size = (800, 600)
     screen = pygame.display.set_mode(screen_size)
 
@@ -43,6 +48,11 @@ def main():
         # Clock
     clock = pygame.time.Clock()
 
+        # Timer for the detection and implementation of the movement fo the drone 
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE, 100)
+
+
     running = True 
     while running:
         
@@ -51,8 +61,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            # Key combinations and movement correlation
-            key =  pygame.key.get_pressed()
+
+            if event.type == SCREEN_UPDATE:
+                drone.move_drone()
+
             
         
         
